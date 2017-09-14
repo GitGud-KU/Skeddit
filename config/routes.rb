b7a040b295170cea00f09f9d1713bf500d8e5093
Mason Wilde <masonwilde@ku.edu>
2017-09-14 14:40:54 -0500

    Add Bootstrap to scss imports

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events
+
+  root 'events#index'
 end
