882e8ea44663e9da98fb3a1514d5298088f03ade
zbruennig <zkb@ku.edu>
2017-09-14 23:12:45 -0500

    More controller things

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,8 +13,10 @@ class EventsController < ApplicationController
 
   # Create an instance var for a new event for the events#new pages
   def new
-    @event = Event.new
+    @event = Event.new(event_params)
     @event.user_id = current_user.id
+    @event.name = current_name
+    @event.date = current_date
   end
 
   # Define what to do when create a new event
@@ -25,6 +27,22 @@ class EventsController < ApplicationController
     else
       render :new
     end
+  end
+
+  #Return edit event form?
+  def edit
+
+  end
+
+  #Define what to do when updating an event
+  def update
+
+  end
+
+  #Define what to do when deleting an event
+  def destroy
+    @event = nil #?
+    redirect_to(events_path)
   end
 
   private
