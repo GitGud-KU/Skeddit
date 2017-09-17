792ca1a64a083b8b1fc697658a3ddde6292b786c
Andy <amonroe805@ku.edu>
2017-09-17 17:31:11 -0500

    Remove unused test files

--- test/controllers/availabilities_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AvailabilitiesControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
-    get availabilities_edit_url
-    assert_response :success
-  end
-
-end
