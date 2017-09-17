9671c3b688479138ce4fa57376fd4313a422a9fd
Zach Bruennig <zkb@ku.edu>
2017-09-17 00:19:45 -0500

    Added relevant validation changes
    Routing when validation fails should be working now.

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,6 +1,12 @@
 class AvailabilitiesController < ApplicationController
+
+  #Simply redirects back to home page. Required since refreshing after a failed validation will bring you to this index.
+  def index
+    redirect_to (events_path)
+  end
+
   def new
-      @availability = Availability.find_or_initialize_by_event_id_and_user_id(params[:event_id], current_user.id)
+    @availability = Availability.find_or_initialize_by_event_id_and_user_id(params[:event_id], current_user.id)
   end
 
   def create
@@ -8,7 +14,8 @@ class AvailabilitiesController < ApplicationController
     if @availability.save
       redirect_to(event_path(@availability.event_id))
     else
-      render :repond
+      #If resources don't load properly upon rerender, add things here.
+      render :new
     end
   end
 
@@ -22,3 +29,4 @@ class AvailabilitiesController < ApplicationController
     params.require(:availability).permit(:event_id,:user_id,:times_available => [])
   end
 end
+
