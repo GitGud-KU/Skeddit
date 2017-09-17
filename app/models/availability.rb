bdd8e4e86e4f524a555b8ea6a69283f008d48cd2
Zach Bruennig <zkb@ku.edu>
2017-09-17 15:42:47 -0500

    Refactor and comment validation

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,16 +1,3 @@
-#Ensures the availability meets all the requirements before being added.
-#Because of the way we set up our database, User and Event ids are already validated elsewhere.
-class AvailabilityValidator < ActiveModel::Validator
-	def validate(availability)
-		#Removes any nils from times_available array
-		availability.times_available = availability.times_available.compact
-		#Make sure at least one available time is selected.
-		if availability.times_available.size <= 0
-			availability.errors[:base] << "Must choose at least one time slot"
-		end
-	end
-end
-
 class Availability < ApplicationRecord
   # Each Availablity 'belongs to' an event and a user.
   # If an availability's user or event is destroyed, so is the availability. 
@@ -21,15 +8,22 @@ class Availability < ApplicationRecord
   before_save :compact_times_allowed
 
   #Ensures the availability meets all the requirements before being added.
-  validates_with AvailabilityValidator
+  #Because of the way we set up our databse, User and Event ids are already validated elsewhere.
+  validate :must_choose_at_least_one_time
 
   private
 
+  #Make sure at least one available time is selected.
+  def must_choose_at_least_one_time
+	self.times_available = self.times_available.compact
+	if availability.times_available.size <= 0
+		availability.errors[:base] << "Must choose at least one time slot"
+	end
+  end
+	
   # Delete all nil values from times_available array using ruby's compact method.
   def compact_times_allowed
     self.times_available = self.times_available.compact
   end
 
 end
-
-
