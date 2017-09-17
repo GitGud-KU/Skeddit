98067bb59f91e6074f5037501d44c034df05d720
Gage Benne <gagedbenne@gmail.com>
2017-09-17 13:01:43 -0500

    Creating availabilities is now working!  Edit is not...

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,4 +1,6 @@
 class AvailabilitiesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_format
 
   #Simply redirects back to home page. Required since refreshing after a failed validation will bring you to this index.
   def index
@@ -6,7 +8,8 @@ class AvailabilitiesController < ApplicationController
   end
 
   def new
-    @availability = Availability.find_or_initialize_by_event_id_and_user_id(params[:event_id], current_user.id)
+    @availability = Availability.find_or_initialize_by(event_id: params[:event_id], user_id: current_user.id)
+    @times_allowed = @availability.event.times_allowed.map(&:to_datetime)
   end
 
   def create
@@ -19,6 +22,17 @@ class AvailabilitiesController < ApplicationController
     end
   end
 
+  def update
+    @availability = Availability.find(event_id: params[:event_id], user_id: current_user.id)
+    if @availability.save
+      redirect_to (events_path)
+    else
+      #If resources don't load properly upon rerender, add things here.
+      render :new
+    end
+
+  end
+
   def destroy
 
   end
@@ -28,5 +42,8 @@ class AvailabilitiesController < ApplicationController
   def availability_params
     params.require(:availability).permit(:event_id,:user_id,:times_available => [])
   end
-end
 
+  def check_format
+    @hour_format = session[:hour_format] || 12
+  end
+end
