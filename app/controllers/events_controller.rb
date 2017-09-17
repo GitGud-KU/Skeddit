f00e484d85b7abdbdf46e437d9e1874f5cbd7c13
Andy <amonroe805@ku.edu>
2017-09-17 12:52:39 -0500

    Implement events#destroy

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -32,6 +32,12 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy if @event.owner == current_user
+    redirect_to(events_path)
+  end
+
   private
 
   # Define the permitted params for creating a new event
