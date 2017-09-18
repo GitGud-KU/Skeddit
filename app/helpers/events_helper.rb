fec3de4f25360dac4d6ff21e07e6bdc72c6d4d2b
Andy <amonroe805@ku.edu>
2017-09-17 21:45:15 -0500

    Comment pre/post conditions for models, helpers, and extensions

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,8 @@
 module EventsHelper
 
   # Get a list of the participants available for a specified event at a specified time.
+  # PRE: all param objects are valid
+  # POST: None
   def available_participants(participants,event,time)
     participants.select{|p| p.is_available_at_event_and_time?(@event,time) }
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -13,39 +13,49 @@ class Event < ApplicationRecord
   # Ensures data is in an allowable format before adding it to the database
   validate :name_cannot_be_empty, :must_choose_at_least_one_time, :date_cannot_be_in_the_past
   
+  # Array of DateTime values iterating through every 30 minutes on today's date starting from midnight.
   POSSIBLE_TIMES_CONST = Array.new(48).map.with_index{|x,index| Date.today.to_datetime + index * (1.0/48)}
 
   private
 
   # Delete all nil values from times_allowed array using ruby's compact method.
+  # PRE: None
+  # POST: Nil values removed from times_allowed
   def compact_times_allowed
     self.times_allowed = self.times_allowed.compact
   end
   
-  #Ensures the name field is not empty
+  # Ensures the name field is not empty
+  # PRE: name exists
+  # POST: If name is blank, add an error to errors hash.
   def name_cannot_be_empty
     if self.name == ""
       self.errors[:base] << "Name cannot be empty"
     end
   end
   
-  #Ensures that at least one time is chosen for the event
+  # Ensures that at least one time is chosen for the event
+  # PRE: times_allowed exists
+  # POST: If times_allowed is empty, add an error to errors hash.
   def must_choose_at_least_one_time
-    self.times_allowed = self.times_allowed.compact
-    if self.times_allowed.size <= 0
+    if compact_times_allowed.size <= 0
       self.errors[:base] << "Must choose at least one time slot"
     end
   end
   
-  #Verifies event date is not before today
+  # Verifies event date is not before today
+  # PRE: date exists
+  # POST: If date is earlier than today, add an error to errors hash.
   def date_cannot_be_in_the_past
     if self.date < Date.today
       self.errors[:base] << "Cannot choose a date in the past"
     end
   end
 
-  #When we created the times, we used today to generate the intervals. 
-  #This changes the date of the datetime to the event date.
+  # When we created the times, we used today to generate the intervals. 
+  # This changes the date of the datetime to the event date.
+  # PRE: times_allowed exists
+  # POST: Set times_allowed to 
   def ensure_times_allowed_has_correct_date
     self.times_allowed = self.times_allowed.map{|time| time.change(:year => self.date.year,
                                               :month => self.date.month,
